8c698f8f30f6a2f2b9382668aed1aee5ef5dde9e
Benjin Yap <benjyap.dev@gmail.com>
2015-04-29 23:48:48 -0400

    ported logicroomchange and logicbackupoptionlist

---
+++ Entities/Logic/Actions/LogicRoomChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyGameEngine.Entities.Logic.Actions {
+	public class LogicRoomChange:LogicNode {
+		public string ZoneId;
+
+		public override LogicNode Clone (LogicNode parent) {
+			LogicRoomChange clone = (LogicRoomChange) base.Clone (parent);
+			clone.ZoneId = this.ZoneId;
+			return clone;
+		}
+	}
+}
---
+++ Entities/Logic/Flow/LogicBackUpOptionList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyGameEngine.Entities.Logic.Flow {
+	public class LogicBackUpOptionList:LogicNode {
+		public int Times = 0;
+
+		public override LogicNode Clone (LogicNode parent) {
+			LogicBackUpOptionList clone = (LogicBackUpOptionList) base.Clone (parent);
+			clone.Times = this.Times;
+			return clone;
+		}
+	}
+}
