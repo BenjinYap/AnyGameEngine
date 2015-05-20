ad314bba62706c8f1aa2e129433bfd2e9bd90803
Benjin Yap <benjyap.dev@gmail.com>
2015-05-19 22:20:45 -0400

    init

---
+++ Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyGameEngine.Entities {
+	public class Item:Entity {
+		public string Name;
+
+		public override string ToString () {
+			return string.Format ("Item, Id: {0}, Name: {1}", this.Id, this.Name);
+		}
+	}
+}
