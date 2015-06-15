7716f791cdc859a4c98f5435351bf490e2897a4f
Benjin Yap <benjyap.dev@gmail.com>
2015-06-14 23:16:09 -0400

    wpf-fied itemstack

--- Modules/Items/ItemStack.cs
+++ Modules/Items/ItemStack.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace AnyGameEngine.Modules.Items {
-	public class ItemStack {
+	public class ItemStack:INotifyPropertyChanged {
+		public event PropertyChangedEventHandler PropertyChanged;
+
 		public string ItemId;
-		public int Quantity;
+		
+		public int Quantity {
+			get { return this.quantity; }
+			set {
+				if (this.quantity != value) {
+					this.quantity = value;
+					this.NotifyPropertyChanged ();
+				}
+			}
+		}
+
+		private int quantity; 
 
 		public ItemStack (string itemId) {
 			this.ItemId = itemId;
@@ -15,6 +30,12 @@ namespace AnyGameEngine.Modules.Items {
 		public ItemStack (string itemId, int quantity) {
 			this.ItemId = itemId;
 			this.Quantity = quantity;
+		}
+
+		private void NotifyPropertyChanged ([CallerMemberName] string propertyName = null) {
+			if (this.PropertyChanged != null) {
+				this.PropertyChanged (this, new PropertyChangedEventArgs (propertyName));
+			}
 		}
 	}
 }
