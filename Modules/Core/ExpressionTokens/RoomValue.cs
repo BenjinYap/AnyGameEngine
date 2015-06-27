b049bba64a077dea29be253acff7e9f0fcbc716b
Benjin Yap <benjyap.dev@gmail.com>
2015-06-27 00:22:15 -0400

    forgot

---
+++ Modules/Core/ExpressionTokens/RoomValue.cs
@@ -0,0 +1,27 @@
+﻿
+
+using AnyGameEngine.Modules.Expressions;
+using System;
+using System.Xml;
+namespace AnyGameEngine.Modules.Core.ExpressionTokens {
+	public class RoomValue:ExpressionToken {
+		private string id;
+		private Property property;
+
+		public RoomValue (XmlNode node) {
+			XmlAttributeCollection attrs = node.Attributes;
+			this.id = attrs ["id"].Value;
+			this.property = (Property) Enum.Parse (typeof (Property), attrs ["property"].Value);
+		}
+
+		public override string Evaluate () {
+			if (this.property == Property.Name) {
+
+			}
+
+			return "1";
+		}
+
+		public enum Property { Name };
+	}
+}
